9de3b9a8087ff979f3ae926d69d08d918024d627
leeanna carter <gy17l2c@leeds.ac.uk>
2019-08-29 19:19:00 +0100

    found the location of the pub and plot it on the town environment

--- python/src/unpackaged/abm/Assingment_two/Drunk_town.py
+++ python/src/unpackaged/abm/Assingment_two/Drunk_town.py
@@ -8,8 +8,9 @@ import matplotlib.pyplot
 # created a empty town list to convert the csv file to a 2d list to plot on a grid.
 drunk_agents = 25
 num_of_houses = 25
-houses = []
+house_num = []
 town = []
+pub = []
 counter = 1    # used a counter to map 25 houses.
 
 # Opens and read in the csv datafile of the town environment.
@@ -27,14 +28,25 @@ print(town)
 file.close()
 
 
-# while count reaches to 25, vales from (10x25) will be added to the empty houses list
+# while count reaches to 25, values from (10x25) are added to the empty houses list.
+# list or door numbers.
 while counter <= num_of_houses:
     interval = 10
-    houses.append(interval * counter)
-    counter +=1
-print(houses)
+    house_num.append(interval * counter)
+    counter +=1  # Iterates the loop by 1 until it reaches 25.
+print(house_num)
 
+#loops through the row of the town file, the the values in the row to find coordinates /n
+#relating to the binary 1's and append to the pub list.
+for y, row in enumerate(town):
+    for x, value in enumerate(row):
+        if value == 1:
+            pub.append([y, x])
+print(pub)  
 
-#plots and show the town's 2d list environment as list is a raster file.  
+#plots the values to show the towns' environment as file is a raster image.
+#plots the location of the pub.
+#shows the raster image.  
 matplotlib.pyplot.imshow(town)
+matplotlib.pyplot.scatter(pub[0][0], pub[0][1])
 matplotlib.pyplot.show() 
