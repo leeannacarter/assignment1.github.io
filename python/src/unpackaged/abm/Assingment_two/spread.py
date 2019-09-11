f939763c271ff17af353ea9e6f4e4bbb20ca29db
leeanna carter <gy17l2c@leeds.ac.uk>
2019-09-11 21:13:03 +0100

    updated the function to move the bacteria

--- python/src/unpackaged/abm/Assingment_two/spread.py
+++ python/src/unpackaged/abm/Assingment_two/spread.py
@@ -2,29 +2,50 @@
 import random
 
 class Agent:
-    def __init__(self, environment, bomb, bacteria):
+    def __init__(self, environment, bomb, bacteria, building_height):
         self.y = bomb[0][0]
         self.x = bomb[0][1]
         self.environment = environment
         self.bacteria = bacteria
+        self.z = building_height
         self.store = 0
 
 # Returns the outcome of the x and y variables.   
     def get_yx(self):
         return self.y, self.x
     
-# Moves the y and x agents twice.
+# Moves the y and x agents.
     def spread(self):
-        move = random.random
         
-        if move <= 0.05:
-            self.x = (self.x - 1) % 300 # move West
+        if random.random() <= 0.75:
+            self.x = (self.x + 1) % 300 # move East by 75%
 
-        elif move > 0.05 and <= 0.15:
-            self.y = (self.y + 1) % 300 # move North or South
+        elif random.random() > 0.75 and random.random() <= 0.85:
+            self.y = (self.y + 1) % 300 # move North or South by 10%
         
-        elif move > 0.15 and <= 0.25:
-            self.y = (self.y - 1) % 300 # move East
+        elif random.random() > 0.85 and random.random() <= 0.95:
+            self.y = (self.y - 1) % 300 # move East by 5%
         
-        else 
-            self.x = (self.x + 1) % 300  
+        else:
+            self.x = (self.x - 1) % 300  
+
+
+    def height(self):
+        
+        if self.z <= 75: # bacteria will not fall
+            self.z -= 1
+            
+        else:
+            turbulance = random.random()
+
+            if turbulance <= 0.7: # 70% the bacteria will fall
+                self.z -= 1
+            
+            elif turbulance > 0.7 and turbulance <= 0.9: #20% the bacteria will rise.
+                self.z += 1
+                
+            else:
+                self.z = self.z # the bacteria will stay at the same level.
+    
+
+        
